17741a307a2f743043a783d345c980fb82cfec6f
ali88367 <ali88367a@gmail.com>
2024-10-29 18:20:40 +0500

    first commit

---
+++ lib/const/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const redColor = Color.fromRGBO(217, 66, 71, 1);
+const blueColor = Color.fromRGBO(10, 42, 58, 1);
+
---
+++ lib/const/images.dart
@@ -0,0 +1,10 @@
+class AppImages {
+  static const quanderyLogo = 'assets/images/BioMedica_splash_screen_image.png';
+  static const quanderySplash = 'assets/images/onboarding_ellipse.png';
+  static const onboard1 = 'assets/images/ellipse2.png';
+  static const onboard2 = 'assets/images/ellipse2.png';
+  static const onboard3 = 'assets/images/ellipse2.png';
+  static const onboard4 = 'assets/images/ellipse2.png';
+  static const onboard5 = 'assets/images/ellipse2.png';
+
+}
